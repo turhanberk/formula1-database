59588c063b5d78e790295913a0178fa45a053f16
turhanberk <berkturhan@sabanciuniv.edu>
2022-07-26 00:23:55 +0300

    db update

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
+<!-- $db = mysqli_connect('localhost','root','','formula1_2021');
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+-->
+
 <?php
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -8,5 +14,5 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
